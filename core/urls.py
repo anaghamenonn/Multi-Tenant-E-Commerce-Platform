bd5652effca1e13283075ed9300febe1a6635b53
Anagha PH <anagha.menon@deeptrace.in>
2025-10-25 15:50:19 +0530

    changes in urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "ok", "message": "Django backend running successfully on Render!"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
 ]
